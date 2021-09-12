183c0c5d85758da87b7fc6caa592298d5962b89d
Andrey Klochok <andreyklochok@gmail.com>
2021-09-12 18:03:01 +0300

    init

---
+++ src/DivDrawer/PileDrawer.js
@@ -0,0 +1,36 @@
+import Drawer from './Drawer.js';
+
+class PileDrawer {
+  constructor(drawer) {
+    this._drawer = Drawer.assert(drawer);
+  }
+
+  draw(parent, pile, y, x) {
+    let m = this._drawer.m;
+    let e = this.importNode(parent, '.Pile');
+    e.style.left  = (0 + x*m) + 'px';
+    e.style.top   = (0 + y*m) + 'px';
+    e.classList.add(pile.direction.name);
+    this.drawCards(e, pile);
+    return e;
+  }
+
+  drawCards(parent, pile) {
+    let size = pile.size ? pile.size : 1;
+    for (let i=0;i<size;i++) {
+      this._drawer.draw(parent, pile.get(i), i*0.2, i*0.2);
+    }
+  }
+
+  importNode(parent, selector) {
+    return this._drawer.importNode(parent, this.fragment, selector);
+  }
+
+  get fragment()  { return this._drawer.getFragment(HTML); }
+}
+
+const HTML = `
+    <div class="Pile"></div>
+`;
+
+export default PileDrawer;
